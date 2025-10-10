b7e28cfa3a9790ad9dd9725ea45e3436e28ce79c
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-10-10 10:37:11 +0200

    Recipe Screen_V1

---
+++ Pasta La Vista!/Assets/Scripts/Kitchen/Recipes.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class Recipes : MonoBehaviour
+{
+    public FPController fPController;
+    bool recipeActive = false;
+    bool playerInTrigger = false;
+    public GameObject recipes;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        recipes.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (playerInTrigger && fPController.interactPressed)
+        {
+            recipeActive = true;
+        }
+
+        if(recipeActive)
+        {
+            Time.timeScale = 0f;
+            recipes.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            fPController.lookSensitivity = 0f;
+        }
+
+    }
+    void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.name == "Player_Francesco")
+        {
+            playerInTrigger = true;
+        }
+    }
+    void OnTriggerExit(Collider trigger)
+    {
+        if (trigger.gameObject.name == "Player_Francesco")
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    public void Resume()
+    {
+        playerInTrigger = false;
+        recipeActive = false;
+        Time.timeScale = 1f;
+        recipes.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        fPController.lookSensitivity = 2f;
+
+    }
+    
+    IEnumerator trigger()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+}
