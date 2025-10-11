3888a8d9e1ad0566fbd3dc37e2db183dab8e7f0a
Sashimi-S <sashsookmungal@gmail.com>
2025-10-11 22:37:31 +0200

    Completed Pizza Machine System

--- Pasta La Vista!/Assets/Scripts/Pizza Machine System/Conveyer 1.cs
+++ Pasta La Vista!/Assets/Scripts/Pizza Machine System/Conveyer 1.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour
+public class ConveyorBelt1 : MonoBehaviour
 {
     public float beltSpeed = 2f;
 
     void OnTriggerStay(Collider other)
     {
-        // Move any pizza object forward
+        
         if (other.CompareTag("Pizza"))
         {
             other.transform.Translate(Vector3.forward * beltSpeed * Time.deltaTime);
